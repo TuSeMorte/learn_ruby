1c20a5bdc95c8defe5bacd1c55bc37f2d6ea7393
TuSeMorte <TuSeMorte@gmail.com>
2019-09-04 12:39:01 -0400

    Complete 00 and 01 lessons

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+def ftoc temp
+    !#puts (temp - 32) * 5/9.0
+    return (temp - 32) * 5/9.0
+end
+def ctof temp
+    !#puts (temp * 9/5.0) + 32
+    return (temp * 9/5.0) + 32
+end
