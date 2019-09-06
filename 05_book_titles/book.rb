69d94b4765ed10124de34f3fa18b6f2ef5b33bd8
TuSeMorte <TuSeMorte@gmail.com>
2019-09-06 12:26:09 -0400

    Finished Code for this exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
 # write your code here
+    def title
+        @title
+    end
+    def title=(title)
+        small_words = ["and","the","over","in","of","a","an"]
+        title = title.split()
+        i = 0
+        while i < title.length
+            if !small_words.include?(title[i]) || i == 0
+                title[i] = title[i].capitalize
+            end
+            i = i + 1
+        end
+        
+        @title = title.join(' ')
+    end
 end
